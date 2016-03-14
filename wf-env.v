5e91f10f68afb26a6f4d328ea17d75201bc66598
alex <alex@example.com>
2016-03-15 00:07:42 +0100

    Added more well-formedeness rules.

--- wf-env.v
+++ wf-env.v
@@ -29,8 +29,40 @@ Section Preservation.
 
   Parameter P: Program.
 
+  Print heap_typeof.
+
   Definition subtypeP := subtype P.
 
-  Definition
+  Local Open Scope type_scope.
 
-  
+  Print sum.
+
+  Definition WF_Var (H: Heap_type) (Gamma: Gamma_type) (L: Env_type) (x: VarName_type):=
+    (p_env.func L x = Some envNull) +
+    {C_o |
+     match C_o with
+       | (C, o) => {witn |
+                    p_gamma.func Gamma x = Some (typt_class C) /\
+                    subtypeP (typt_class (heap_typeof H o witn)) (typt_class C)
+                   }
+     end} +
+    {C_o |
+     match C_o with
+       | (C, o) => {witn |
+                    p_gamma.func Gamma x = Some (typt_box C) /\
+                    subtypeP (typt_class (heap_typeof H o witn)) (typt_class C)
+                   }
+     end}.
+
+  Definition WF_Env H Gamma L :=
+    gamma_env_subset Gamma L *
+    forall x sigma,
+      p_gamma.func Gamma x = Some sigma ->
+      WF_Var H Gamma L x.
+
+  Inductive T_Frame1 : Heap_type -> ann_frame_type -> typecheck_type -> Type :=
+  | t_frame : forall H Gamma eff t L ann sigma,
+                isTerm t ->
+                TypeChecks Gamma eff t sigma ->
+                WF_Env H Gamma L ->
+                T_Frame1 H (ann_frame (sframe L t) ann) sigma.
